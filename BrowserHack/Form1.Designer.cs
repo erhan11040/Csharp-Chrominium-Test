012ac588f3ad7e46cf9c24a4b15ea9c37a83c834
HeathCliff <erhan11040@gmail.com>
2019-01-12 21:18:57 +0300

    Project Uploaded

---
+++ BrowserHack/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BrowserHack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(728, 129);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(35, 13);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "label1";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(713, 45);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 419);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ BrowserHack/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+namespace BrowserHack
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [PermissionSetAttribute(SecurityAction.InheritanceDemand, Name = "FullTrust")]
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+       
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+
+            try
+            {
+               /* browser.Document.GetElementById("btnClear").InvokeMember("click");
+                browser.Document.GetElementById("btn").InvokeMember("click");
+                string sonuc=browser.Document.GetElementById("speech").InnerHtml;
+                lbl.Text = sonuc;*/
+            }
+            catch (Exception ex)
+            {
+                string exmes = ex.Message;
+            }
+            
+        }
+
+        private void btn_click(object sender, EventArgs e)
+        {
+            /*browser.Document.GetElementById("btn").InvokeMember("click");
+            string sonuc = browser.Document.GetElementById("speech").InnerHtml;*/
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Start the browser after initialize global component
+            InitializeChromium();
+            // Register an object in javascript named "cefCustomObject" with function of the CefCustomObject class :3
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chromeBrowser.ShowDevTools();
+            
+
+        }
+
+        public void InitializeChromium()
+        {
+            
+
+            // Note that if you get an error or a white screen, you may be doing something wrong !
+            // Try to load a local file that you're sure that exists and give the complete path instead to test
+            // for example, replace page with a direct path instead :
+            // String page = @"C:\Users\SDkCarlos\Desktop\afolder\index.html";
+
+            //String page = @"C:\Users\SDkCarlos\Desktop\artyom-HOMEPAGE\index.html";
+
+
+            // Initialize cef with the provided settings
+            CefSettings settings = new CefSettings();
+
+            // Initialize cef with a command line argument
+            // In this case the enable-media-stream flag that allows you to access the camera and the microphone
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+            settings.CefCommandLineArgs.Add("allow-running-insecure-content", "1");
+            settings.CefCommandLineArgs.Add("use-fake-ui-for-media-stream", "1");
+            settings.CefCommandLineArgs.Add("enable-usermedia-screen-capture", "1");
+            Cef.Initialize(settings);
+            // Create a browser component      q
+            chromeBrowser = new ChromiumWebBrowser("https://www.google.com");
+
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            // Allow the use of local resources in the browser
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chromeBrowser.BrowserSettings = browserSettings;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
